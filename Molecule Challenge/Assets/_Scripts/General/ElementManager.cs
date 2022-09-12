9e039729321bcbf21193dcd0dab2ddc81186bcbc
kalmcam <cam.peele@kalmstudios.com>
2022-09-12 14:20:46 -0400

    Implemented dynamic creation of ElementToggles

--- Molecule Challenge/Assets/_Scripts/General/ElementManager.cs
+++ Molecule Challenge/Assets/_Scripts/General/ElementManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ElementManager : MonoBehaviour
 {
@@ -16,8 +17,19 @@ public class ElementManager : MonoBehaviour
         public int Electrons;
     }
 
+    [Header("UI")]
+    [SerializeField] private GameObject m_togglePrefab;
+    [SerializeField] private Color m_colorOne;
+    [SerializeField] private GameObject m_elementPanelOne;
+    [SerializeField] private Color m_colorTwo;
+    [SerializeField] private GameObject m_elementPanelTwo;
+
+    [Header("Element List")]
     [SerializeField] private List<ElementInfo> Elements = new List<ElementInfo>();
     public Dictionary<string, ElementInfo> ElementDict = new Dictionary<string, ElementInfo>();
+
+    private GameObject m_prefab;
+    private List<GameObject> m_elementToggles = new List<GameObject>();
 
     private void Awake()
     {
@@ -26,9 +38,52 @@ public class ElementManager : MonoBehaviour
 
     private void Start()
     {
+        CreateElementDictionary();
+        CreateElementToggles();
+    }
+
+    private void CreateElementDictionary()
+    {
         foreach (ElementInfo element in Elements)
         {
             ElementDict.Add(element.Name, element);
+        }
+    }
+
+    private void CreateElementToggles()
+    {
+        ElementToggle he;
+        foreach (ElementInfo element in Elements)
+        {
+            // Create element toggle for Element Panel One
+            m_prefab = Instantiate(m_togglePrefab, m_elementPanelOne.transform);
+
+            m_prefab.GetComponent<Toggle>().group = m_elementPanelOne.GetComponent<ToggleGroup>();
+
+            he = m_prefab.GetComponent<ElementToggle>();
+            if (he != null)
+            {
+                he.SetSymbolText(element.Symbol);
+                he.SetHighlightGameObject(element.HighlightObject);
+                he.SetHighlightColor(m_colorOne);
+            }
+
+            m_elementToggles.Add(m_prefab);
+
+            // Create element toggle for Element Panel Two
+            m_prefab = Instantiate(m_togglePrefab, m_elementPanelTwo.transform);
+
+            m_prefab.GetComponent<Toggle>().group = m_elementPanelTwo.GetComponent<ToggleGroup>();
+
+            he = m_prefab.GetComponent<ElementToggle>();
+            if (he != null)
+            {
+                he.SetSymbolText(element.Symbol);
+                he.SetHighlightGameObject(element.HighlightObject);
+                he.SetHighlightColor(m_colorTwo);
+            }
+
+            m_elementToggles.Add(m_prefab);
         }
     }
 }
---
+++ Molecule Challenge/Assets/_Scripts/UI/ElementToggle.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElementToggle : MonoBehaviour
+{
+    [SerializeField] private Toggle m_toggle;
+    [SerializeField] private Color m_highlightColor;
+    [SerializeField] private GameObject m_highlightGameObject;
+    [SerializeField] private TextMeshProUGUI m_label;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_toggle != null)
+        {
+            m_toggle.onValueChanged.AddListener(delegate { OnValueChanged(m_toggle); });
+        }
+    }
+
+    public void SetSymbolText(string symbol)
+    {
+        m_label.SetText(symbol);
+    }
+
+    public void SetHighlightGameObject(GameObject highlightObj)
+    {
+        m_highlightGameObject = highlightObj;
+    }
+
+    public void SetHighlightColor(Color color)
+    {
+        m_highlightColor = color;
+    }
+
+    private void OnValueChanged(Toggle toggle)
+    {
+        m_highlightGameObject.SetActive(toggle.isOn);
+        if (toggle.isOn)
+        {
+            m_highlightGameObject.GetComponent<Image>().color = m_highlightColor;
+        }
+    }
+}
